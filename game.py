a27e9b5c13cc41ae863352749ad3fa30079e02a9
Anikeeva Diana <diana_box@list.ru>
2020-07-14 23:39:31 +0300

    RockPaperScissors: step 2

--- game.py
+++ game.py
@@ -1,5 +1,14 @@
-# Write your code here
+import random
+
+
 pairs = {'scissors': 'rock', 'rock': 'paper', 'paper': 'scissors'}
 
 user_choice = input()
-print(f'Sorry, but computer chose {pairs[user_choice]}')
+computer_choice = random.choice(list(pairs.keys()))
+
+if user_choice == computer_choice:
+    print(f'There is a draw ({user_choice})')
+elif pairs[user_choice] == computer_choice:
+    print(f'Sorry, but computer chose ({computer_choice})')
+else:
+    print(f'Well done. Computer chose {computer_choice} and failed)')
