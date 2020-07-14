e52de22f38554be6756e54a240b2f3539c341ff8
Anikeeva Diana <diana_box@list.ru>
2020-07-14 23:56:16 +0300

    RockPaperScissors: step 3

--- game.py
+++ game.py
@@ -3,12 +3,28 @@ import random
 
 pairs = {'scissors': 'rock', 'rock': 'paper', 'paper': 'scissors'}
 
-user_choice = input()
-computer_choice = random.choice(list(pairs.keys()))
 
-if user_choice == computer_choice:
-    print(f'There is a draw ({user_choice})')
-elif pairs[user_choice] == computer_choice:
-    print(f'Sorry, but computer chose ({computer_choice})')
-else:
-    print(f'Well done. Computer chose {computer_choice} and failed)')
+def print_rps_result(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        print(f'There is a draw ({user_choice})')
+    elif pairs[user_choice] == computer_choice:
+        print(f'Sorry, but computer chose ({computer_choice})')
+    else:
+        print(f'Well done. Computer chose {computer_choice} and failed)')
+
+
+while True:
+    user_choice = input()
+
+    if user_choice == '!exit':
+        print('Bye!')
+        break
+
+    if user_choice not in pairs:
+        print('Invalid input')
+        break
+
+    computer_choice = random.choice(list(pairs.keys()))
+    print_rps_result(user_choice, computer_choice)
+
+
