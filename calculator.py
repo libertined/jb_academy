18e02ba1f8d876b261ca019a0ba6ed6b9249a4b9
Anikeeva Diana <diana_box@list.ru>
2020-08-11 17:46:32 +0300

    Calculator: step 2

--- calculator.py
+++ calculator.py
@@ -1,3 +1,12 @@
 # write your code here
-numbers = input().split(' ')
-print(int(numbers[0]) + int(numbers[1]))
+
+while True:
+    command = input()
+    if command == '/exit':
+        print('Bye!')
+        break
+    numbers = command.split(' ')
+    if len(numbers) == 1 and numbers[0] != '':
+        print(numbers[0])
+    elif len(numbers) == 2:
+        print(int(numbers[0]) + int(numbers[1]))
