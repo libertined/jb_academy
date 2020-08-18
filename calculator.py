35fc2a2561290398b45db9a483bb09f1b142f6ba
Anikeeva Diana <diana_box@list.ru>
2020-08-18 16:14:59 +0300

    Calculator: step 4

--- calculator.py
+++ calculator.py
@@ -1,4 +1,31 @@
 # write your code here
+def get_digit(number):
+    return int(number.replace(' ', ''))
+
+
+def get_numbers(input_string):
+    result = []
+    number = ''
+    for i in input_string:
+        if i == '+':
+            if number == '-':
+                continue
+            elif number != '':
+                result.append(get_digit(number))
+                number = ''
+            continue
+        elif i == '-':
+            if number == '-':
+                number = ''
+                continue
+            elif number != '':
+                result.append(get_digit(number))
+                number = ''
+        number += i
+    if number != '':
+        result.append(get_digit(number))
+    return result
+
 
 while True:
     command = input()
@@ -6,10 +33,10 @@ while True:
         print('Bye!')
         break
     elif command == '/help':
-        print('The program calculates the sum of numbers')
+        print('The program calculates the sum of positive and negative numbers')
         continue
     elif command == '':
         continue
 
-    numbers = [int(x) for x in command.split(' ')]
+    numbers = get_numbers(command)
     print(sum(numbers))
